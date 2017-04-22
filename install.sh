116ca2564d2a8249a4676e74d1ce825541f7e221
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2017-04-23 01:30:58 +0300

    install.sh updated

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
+git clone git@github.com:SiarheiFedartsou/XcodeTemplates.git
+cd XcodeTemplates
 mkdir -p ~/Library/Developer/Xcode/Templates/File\ Templates/Source/Screen\ Module.xctemplate
 cp -R ./Screen\ Module.xctemplate/. ~/Library/Developer/Xcode/Templates/File\ Templates/Source/Screen\ Module.xctemplate
+cd ..
+rm -r XcodeTemplates
